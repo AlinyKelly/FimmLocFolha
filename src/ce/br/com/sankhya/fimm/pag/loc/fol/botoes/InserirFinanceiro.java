c0e7eadb1e8d57893cdb665c0f0401518bd0a15c
Aliny Kelly <alinykellyfs@gmail.com>
2024-08-14 14:01:26 -0300

    Commit 4 - Aliny - Conclusão do código do InserirFinanceiro

--- src/ce/br/com/sankhya/fimm/pag/loc/fol/botoes/InserirFinanceiro.java
+++ src/ce/br/com/sankhya/fimm/pag/loc/fol/botoes/InserirFinanceiro.java
@@ -7,13 +7,12 @@ import br.com.sankhya.jape.vo.DynamicVO;
 import utilitarios.Utils;
 
 import java.math.BigDecimal;
-import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class InserirFinanceiro implements AcaoRotinaJava {
     @Override
     public void doAction(ContextoAcao contextoAcao) throws Exception {
-        // Pegar a empresa para a TGFFIN da Conta do Pagamento
         // DTENTSAI, DHMOV usar igual à DTNEG, DTALTER pegar a data do servidor
         // Usar NUMNOTA sequencial
         // Fixar RECDESP = -1
@@ -31,9 +30,54 @@ public class InserirFinanceiro implements AcaoRotinaJava {
             Timestamp dtpagamento = (Timestamp) linha.getCampo("DTPAG");
             BigDecimal top = (BigDecimal) linha.getCampo("CODTIPOPER");
             BigDecimal natureza = (BigDecimal) linha.getCampo("CODNAT");
+            String tipoConta = (String) linha.getCampo("TIPOCONTA");
+            BigDecimal centroResultado = (BigDecimal) linha.getCampo("CODCENCUS");
 
-            //Criar a TGFFIN
+            LocalDateTime localDateTime = dtpagamento.toLocalDateTime();
+            int mes = localDateTime.getMonthValue() - 1;
+            int ano = localDateTime.getYear();
 
+            String numnota = mes + String.valueOf(ano);
+
+            //Utils.mostraErro("Competencia = " + numnota + " Data Pagamento = " + dtpagamento + " Mes - 1 = " + mes + " ano = " + ano + " Numnota = " + numnota);
+
+            BigDecimal tipoTitulo;
+            DynamicVO buscarConfigTipo = Utils.retornaVO("AD_CONFIGTIP", "TIPOCONTA = '" + tipoConta + "'");
+
+            if (buscarConfigTipo != null) {
+                tipoTitulo = buscarConfigTipo.asBigDecimalOrZero("CODTIPTIT");
+            } else {
+                tipoTitulo = BigDecimal.ZERO;
+            }
+
+            Registro criarFinanceiro = contextoAcao.novaLinha("Financeiro");
+            criarFinanceiro.setCampo("NUMNOTA", numnota);
+            criarFinanceiro.setCampo("CODPARC", parceiro);
+            criarFinanceiro.setCampo("CODEMP", empresaPagamento);
+            criarFinanceiro.setCampo("RECDESP", -1);
+            criarFinanceiro.setCampo("DTVENC", dtVencimento);
+            criarFinanceiro.setCampo("DTNEG", dtNeg);
+            criarFinanceiro.setCampo("DHMOV", dtNeg); //Data e Hora
+            criarFinanceiro.setCampo("DTENTSAI", dtNeg);
+            criarFinanceiro.setCampo("DTALTER", Utils.getDHAtual()); //Data e Hora
+            criarFinanceiro.setCampo("VLRDESDOB", vlrpag);
+            criarFinanceiro.setCampo("CODTIPOPER", top);
+            criarFinanceiro.setCampo("CODTIPTIT", tipoTitulo);
+            criarFinanceiro.setCampo("CODNAT", natureza);
+            criarFinanceiro.setCampo("CODCENCUS", centroResultado);
+            criarFinanceiro.setCampo("CODBCO", bancoPagamento);
+            criarFinanceiro.setCampo("CODCTABCOINT", contaPagamento);
+            criarFinanceiro.setCampo("AD_DTPAG", dtpagamento);
+            criarFinanceiro.save();
+
+            Object nufin = criarFinanceiro.getCampo("NUFIN");
+
+            //Realizar um update no campo nufin das linhas selecionadas.
+            linha.setCampo("NUFIN", nufin);
+            linha.save();
         }
+
+        contextoAcao.setMensagemRetorno("Títulos gerados com sucesso.");
+
     }
 }
